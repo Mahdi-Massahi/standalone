e6cd2932f0d72eff851f108002632f3849489553
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2025-03-04 18:38:52 +0100

    feat: added user's fields

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -3,4 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=20)
+    bio = models.TextField()
+    phone = models.CharField(max_length=32)
+    location = models.CharField(max_length=32)
+    company = models.CharField(max_length=32)
+    linkedin = models.CharField(max_length=128)
+    github = models.CharField(max_length=128)
+    website = models.CharField(max_length=128)
+    summary = models.CharField(max_length=128)
+    languages = models.JSONField(default="")
+    soft_skills = models.JSONField(default="")
+    hard_skills = models.JSONField(default="")
